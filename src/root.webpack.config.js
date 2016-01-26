a2b8141a3c8c13d5c9c3adfa1d7d08e3f93cc24c
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-26 11:25:36 -0800

    Enhance root normalization logic and make idempotent.
    Previously multiple calls to this would keep adding entries to the `root` array due to calls from `webpack-partial`. Now that we have full control of the config structure we just do the merge logic ourselves.
    
    Also now handle the case where `root` in the webpack configuration is something other than a string.

--- src/root.webpack.config.js
+++ src/root.webpack.config.js
@@ -1,10 +1,36 @@
-import partial from 'webpack-partial';
 import path from 'path';
 
-export default (root = 'src') => (config) => partial(config, {
-  resolve: {
-    root: [
-      root.charAt(0) === '/' ? root : path.join(config.context, root),
-    ],
-  },
-});
+const exists = (path, resolve) => {
+  if (Array.isArray(resolve)) {
+    return resolve.some((entry) => exists(path, entry));
+  } else if (typeof resolve === 'function') {
+    return resolve(path);
+  }
+  return path === resolve;
+};
+
+const append = (path, resolve) => {
+  if (Array.isArray(resolve)) {
+    return [...resolve, path];
+  }
+  return [resolve, path];
+};
+
+export default (root = 'src') => (config) => {
+  const entry = root.charAt(0) === '/' ? root : path.join(config.context, root);
+  if (!config.resolve || !config.resolve.root) {
+    return {
+      ...config,
+      resolve: {root: [entry]},
+    };
+  } else if (config.resolve.root && exists(entry, config.resolve.root)) {
+    return config;
+  }
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      root: append(entry, config.resolve.root),
+    },
+  };
+};
